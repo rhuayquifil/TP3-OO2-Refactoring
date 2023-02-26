0c44d2b7d4d5aae216d09ffb49d8b7d00f749fdf
Enrique Pablo Molinari <enrique.molinari@gmail.com>
2023-02-26 16:32:57 -0300

    saco el findBy que tenia unos continue feos

--- test/ar/unrn/eje2/RecaudacionTest.java
+++ test/ar/unrn/eje2/RecaudacionTest.java
@@ -1,16 +1,11 @@
 package ar.unrn.eje2;
 
 import static org.junit.Assert.assertEquals;
-
 import java.io.IOException;
 import java.util.HashMap;
 import java.util.Map;
-
 import org.junit.Assert;
 import org.junit.Test;
-
-import ar.unrn.eje2.Recaudacion;
-import ar.unrn.eje2.NoSuchEntryException;
 
 public class RecaudacionTest {
 
@@ -76,70 +71,5 @@ public class RecaudacionTest {
     assertEquals(row.get("raised_amount"), "500000");
     assertEquals(row.get("round"), "angel");
 
-  }
-
-  @Test
-  public void testFindByGivenCompanyName() throws IOException, NoSuchEntryException {
-    Map<String, String> options = new HashMap<String, String>();
-    options.put("company_name", "Facebook");
-    Map<String, String> row = Recaudacion.findBy(options);
-
-    assertEquals(row.get("permalink"), "facebook");
-    assertEquals(row.get("company_name"), "Facebook");
-    assertEquals(row.get("number_employees"), "450");
-    assertEquals(row.get("category"), "web");
-    assertEquals(row.get("city"), "Palo Alto");
-    assertEquals(row.get("state"), "CA");
-    assertEquals(row.get("funded_date"), "1-Sep-04");
-    assertEquals(row.get("raised_amount"), "500000");
-    assertEquals(row.get("round"), "angel");
-  }
-
-  @Test
-  public void testFindByGivenState() throws IOException, NoSuchEntryException {
-    Map<String, String> options = new HashMap<String, String>();
-    options.put("state", "CA");
-    Map<String, String> row = Recaudacion.findBy(options);
-
-    assertEquals(row.get("permalink"), "digg");
-    assertEquals(row.get("company_name"), "Digg");
-    assertEquals(row.get("number_employees"), "60");
-    assertEquals(row.get("category"), "web");
-    assertEquals(row.get("city"), "San Francisco");
-    assertEquals(row.get("state"), "CA");
-    assertEquals(row.get("funded_date"), "1-Dec-06");
-    assertEquals(row.get("raised_amount"), "8500000");
-    assertEquals(row.get("round"), "b");
-  }
-
-  @Test
-  public void testFindByMultipleOptions() throws IOException, NoSuchEntryException {
-    Map<String, String> options = new HashMap<String, String>();
-    options.put("company_name", "Facebook");
-    options.put("round", "c");
-    Map<String, String> row = Recaudacion.findBy(options);
-
-    assertEquals(row.get("permalink"), "facebook");
-    assertEquals(row.get("company_name"), "Facebook");
-    assertEquals(row.get("number_employees"), "450");
-    assertEquals(row.get("category"), "web");
-    assertEquals(row.get("city"), "Palo Alto");
-    assertEquals(row.get("state"), "CA");
-    assertEquals(row.get("funded_date"), "1-Oct-07");
-    assertEquals(row.get("raised_amount"), "300000000");
-    assertEquals(row.get("round"), "c");
-  }
-
-  @Test
-  public void testFindByNotExists() throws IOException {
-    Map<String, String> options = new HashMap<String, String>();
-    options.put("company_name", "NotFacebook");
-    options.put("round", "c");
-    try {
-      Map<String, String> row = Recaudacion.findBy(options);
-      Assert.fail("findBy should throw exception");
-    } catch (NoSuchEntryException e) {
-      // Esta bien que salga por el catch
-    }
   }
 }
